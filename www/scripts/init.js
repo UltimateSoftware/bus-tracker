62f61763f626236e8288c256b7a2a032f3c8721e
Joseph Cutrono <joseph_cutrono@ultimatesoftware.com>
2016-06-22 11:45:22 -0400

    adding appropriate chaining to support device ready and then loading google maps api

---
+++ www/scripts/init.js
@@ -0,0 +1,32 @@
+(function (global) {
+    "use strict";
+
+    function onDeviceReady() {
+        document.addEventListener("online", onOnline, false);
+        document.addEventListener("resume", onResume, false);        
+        loadMapsApi();
+    }
+
+    function onOnline() {
+        loadMapsApi();
+    }
+
+    function onResume() {
+        loadMapsApi();
+    }
+
+    function loadMapsApi() {
+        if ((navigator.connection && navigator.connection.type === Connection.NONE) || (global.google !== undefined && global.google.maps)) {
+            return;
+        }
+
+        $.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBZqZxv3WgxC6-dED3DVOzCMLx3aCziY78&libraries=places&callback=onMapsApiLoaded');
+    }
+
+    global.onMapsApiLoaded = function () {
+        initMap();
+    };
+
+    //onDeviceReady();
+    document.addEventListener("deviceready", onDeviceReady, false);
+})(window);
