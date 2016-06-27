8d6331032f62cc745ab9bf46ab4af64d178ae3a5
Ben Diuguid <bendiuguid@gmail.com>
2016-06-27 16:59:33 -0400

    add background-geolocation-sync

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -358,55 +358,7 @@ function checkOutFromBus(watchId) {
       }
 }
 
-document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady () {
-
-    /**
-    * This callback will be executed every time a geolocation is recorded in the background.
-    */
-    var callbackFn = function(location) {
-        console.log('[js] BackgroundGeolocation callback:  ' + location.latitude + ',' + location.longitude);
-
-        var position = {
-          coords: {
-            latitude: location.latitude,
-            longitude: location.longitude
-          }
-        };
-
-        updateCurrentPosition(position);
-
-        // Do your HTTP request here to POST location to your server.
-        // jQuery.post(url, JSON.stringify(location));
-
-        /*
-        IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
-        and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
-        IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
-        */
-        backgroundGeolocation.finish();
-    };
-
-    var failureFn = function(error) {
-        console.log('BackgroundGeolocation error');
-    };
-
-    // BackgroundGeolocation is highly configurable. See platform specific configuration options
-    backgroundGeolocation.configure(callbackFn, failureFn, {
-        desiredAccuracy: 10,
-        stationaryRadius: 20,
-        distanceFilter: 30,
-        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
-        stopOnTerminate: false, // <-- enable this to clear background location settings when the app terminates
-    });
-
-    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
-    backgroundGeolocation.start();
-
-    // If you wish to turn OFF background-tracking, call the #stop method.
-    // backgroundGeolocation.stop();
-}
 /*
 Returns distance in meters
 */
@@ -456,6 +408,7 @@ function updateCurrentPosition(position) {
         });
         if (minDistance > 100) {
             alertModal('Path Error!', 'You\'re off your path, do you need to check out?', 'error');
+            if(window.cordova) window.backgroundGeolocation.stop();
         }
     }
     // remove current positon marker
--- www/scripts/init.js
+++ www/scripts/init.js
@@ -6,6 +6,51 @@
         document.addEventListener("resume", onResume, false);
         loadMapsApi();
         connectToServerSocket();
+
+        /**
+        * This callback will be executed every time a geolocation is recorded in the background.
+        */
+        var callbackFn = function(location) {
+            console.log('[js] BackgroundGeolocation callback:  ' + location.latitude + ',' + location.longitude);
+
+            var position = {
+              coords: {
+                latitude: location.latitude,
+                longitude: location.longitude
+              }
+            };
+
+            updateCurrentPosition(position);
+
+            /*
+            IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+            and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+            IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+            */
+            global.backgroundGeolocation.finish();
+        };
+
+        var failureFn = function(error) {
+            console.log('BackgroundGeolocation error');
+        };
+
+        if(global.cordova) {
+            // BackgroundGeolocation is highly configurable. See platform specific configuration options
+            global.backgroundGeolocation.configure(callbackFn, failureFn, {
+                desiredAccuracy: 10,
+                stationaryRadius: 20,
+                distanceFilter: 20,
+                interval: 60 * 1000,
+                debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+                stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
+            });
+
+            // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+            global.backgroundGeolocation.start();
+
+            // If you wish to turn OFF background-tracking, call the #stop method.
+            // backgroundGeolocation.stop();
+        }
     }
 
     function connectToServerSocket() {
@@ -37,7 +82,12 @@
         initMap();
     };
 
-    //comment out onDeviceReady when deploying - It's the Wild West, baby.
-    onDeviceReady();
+    // may need comment out onDeviceReady when deploying - It's the Wild West, baby.
+    if(!global.cordova) {
+        onDeviceReady();
+    }
     document.addEventListener("deviceready", onDeviceReady, false);
 })(window);
+
+
+document.addEventListener('deviceready', onDeviceReady, false);
